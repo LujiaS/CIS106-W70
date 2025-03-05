cc8eea7671ae7b217b5224ab66a4e69a0fe9f226
LujiaS <luciasun05@gmail.com>
2025-03-05 00:14:00 -0600

    Add files via upload

---
+++ ps2p2.py
@@ -0,0 +1,10 @@
+
+#p2
+lname = input('Enter student last name')
+midterm = float(input('midterm score'))
+final = float(input('final score'))
+#process
+total = midterm + final
+#output
+print('Student last name', lname)
+print('Total points earned', total)
